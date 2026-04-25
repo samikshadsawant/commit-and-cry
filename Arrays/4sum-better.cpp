72f21d2d8c49e6cfe37290ad9a20a5ff476b44b3
samikshadsawant <149319099+samikshadsawant@users.noreply.github.com>
2026-04-25 14:42:18 +0530

    Create 4sum-better.cpp

---
+++ Arrays/4sum-better.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+
+       set<vector<int>> st;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            
+            for(int j=i+1;j<n;j++){
+                set<long long> hashSet;
+                for(int k= j+1;k<n;k++){
+                long long sum=nums[i]+nums[j];
+                sum+=nums[k];
+
+                long long fourth= target-(sum);
+                if(hashSet.find(fourth)!=hashSet.end()){
+                    vector<int> temp={nums[i],nums[j],nums[k],(int)fourth};
+                    sort(temp.begin(),temp.end());
+                    st.insert(temp);
+                }
+                hashSet.insert(nums[k]);
+                }
+                
+            }
+        }
+        vector<vector<int>> ans(st.begin(),st.end());
+        return ans;
+
+        }
+        
+    
+};
