cf4c4138e07939ab7c25263f30c45a27ce048e9a
samikshadsawant <149319099+samikshadsawant@users.noreply.github.com>
2026-04-25 14:29:11 +0530

    Create 4sum-brute.cpp

---
+++ Arrays/4sum-brute.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+
+        set<vector<int>> st;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                for(int k=j+1;k<n;k++){
+                    for(int l=k+1;l<n;l++){
+                    long long sum = (long long)nums[i] + nums[j] + nums[k] + nums[l];
+                    if(sum==target){
+                        vector<int> temp={nums[i],nums[j],nums[k],nums[l]};
+                        sort(temp.begin(),temp.end());
+                        st.insert(temp);
+                    }
+                    }
+                }
+            }
+        }
+        vector<vector<int>> ans(st.begin(),st.end());
+        return ans;
+
+        }
+        
+    
+};
