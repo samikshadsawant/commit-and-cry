f3b4b7f3fad691fa2cd1d8a3bbbf88db3e0884ed
samikshadsawant <149319099+samikshadsawant@users.noreply.github.com>
2026-05-04 23:43:49 +0530

    Create maximum product subarray brute.cpp

---
+++ Arrays/maximum product subarray brute.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int ans=INT_MIN;
+
+        for(int i=0;i<nums.size();i++){
+              int product=1;
+            for(int j=i;j<nums.size();j++){
+                
+                
+
+                    product*=nums[j];
+                    ans=max(ans,product);
+                    
+
+                
+                
+            }
+            
+        }
+        return ans;
+        
+    }
+};
